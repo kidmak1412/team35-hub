cdd0e5b8540172e38b3e1ab0fdb7b86588088a6e
35DA&K-Hub <lohlaem.harit@gmail.com>
2026-03-04 10:43:45 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // <--- เพิ่มบรรทัดนี้
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // <--- เพิ่มบรรทัดนี้
+    tailwindcss()
   ],
+  base: '/team35-hub/', // <--- บรรทัดนี้สำคัญมาก! เปลี่ยน team35-hub เป็นชื่อ Repository ใน GitHub ของคุณ
 })
